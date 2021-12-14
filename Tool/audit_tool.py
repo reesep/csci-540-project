de9f5b09a9269d60f1d29d3a062bdde6de0acea1
reesep <reesepearsallcs@gmail.com>
2021-12-14 16:35:08 -0700

    add hashing algorithm check

--- Tool/audit_tool.py
+++ Tool/audit_tool.py
@@ -6,6 +6,7 @@ import test_for_password_complexity
 import test_for_rainbow_vulnerability
 import test_for_weak_passwords
 import test_for_salt
+import test_for_hashing_algorithm
 
 ### TO DO: Replace .test() with appropriate function name (or change other .py
 ##                                                                                        (files to have .test func)
@@ -16,15 +17,15 @@ Function checks for different for information and database vulnerabilities
 def run_audit_tool():
 
     ## Read in all columns that should be checked
-    config = open("Tool/config.csv","r")
+    config = open("config.csv","r")
     columns_to_check = config.read().strip().split(",") ## convert into list
 
     ## test_for_sql_injection.test() ## TO DO
-    ## test_for_hashing_algorithm.test() ## TO DO
+    test_for_hashing_algorithm.test()
 
     for each in columns_to_check:  
 
-        file = open("Data/user.csv","r")
+        file = open("../Data/user.csv","r")
         columns = file.readline().strip().replace('"',"").split(",")
         index = columns.index(each)
 
--- Tool/test_for_hashing_algorithm.py
+++ Tool/test_for_hashing_algorithm.py
@@ -10,18 +10,17 @@ def test():
         file = open("../Website/" + filename,"r")
 
         text = file.read()
-        print(text)
-        patterns = ["md5(","sha1("]
+        patterns = ["md5","sha1"]
 
         for each in patterns:
 
             if re.search(each,text):
-                print("Hashing algorithm",each,"was detected")
-                print("Consider using a more secure hashing algorithm")
-
+                
+                print(filename,": Hashing algorithm",each,"was detected")
+                print("---->Consider using a more secure hashing algorithm")
                 return
-
-        print("Hashing algorithm was not detected. Please ensure you are using a secure hashing algorithm")
+            
+            print(filename,": Hashing algorithm was not detected. Please ensure you are using a secure hashing algorithm")
             
 
 
